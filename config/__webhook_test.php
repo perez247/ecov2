f3c82fa2809d9b8b00124c2de38afa4a718af802
perez247 <elperez247@gmail.com>
2019-11-20 21:51:16 +0000

    testing

--- config/__webhook_test.php
+++ config/__webhook_test.php
@@ -10,6 +10,8 @@ if ($_SERVER['PHP_AUTH_PW'] == $globalEnv->WEBHOOK_SECRET)
     {
         shell_exec('sudo -E /home/agent/app/config/redeploy.sh > /tmp/redeploy_docker.txt & printf "%u" $!');
         // file_put_contents('/tmp/azure.txt', print_r($_SERVER['PHP_AUTH_PW'], true));
+        // It is very important that you create a friendly environment
+        
     }
 else {
     throw new \Exception("Failed to rebuild");
